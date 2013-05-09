4fa5793fef36f52651d260b6d593abd3081e315b
Chaitanya Vaidya <cvaidya1986@gmail.com>
2013-05-08 20:08:09 -0500

    more editing

--- 37signals/application.js
+++ 37signals/application.js
@@ -4,12 +4,10 @@ $(document).ready(function(){
 	  });
 	  $('#hr').on('mouseenter',function(){
 	    $(this).find('#Headline').text('Highrise remembers the important things about people you’d normally forget.');
-		$('#subHeadline').text('Keep a permanent record of people you do business with. Know who you talked to, when you talked to them, what was said, and when to follow up next. Over 20,000,000 contacts are 
-		    		tracked using Highrise.');
+		$('#subHeadline').text('Keep a permanent record of people you do business with. Know who you talked to, when you talked to them, what was said, and when to follow up next. Over 20,000,000 contacts are tracked using Highrise.');
 		    });
 	   $('#cf').on('mouseenter',function(){
 	     $(this).find('#Headline').text('Basecamp remembers the important things about people you’d normally forget.');
-		$('#subHeadline').text('Keep a permanent record of people you do business with. Know who you talked to, when you talked to them, what was said, and when to follow up next. Over 20,000,000 contacts are 
-		    		tracked using Highrise.');
+		$('#subHeadline').text('Keep a permanent record of people you do business with. Know who you talked to, when you talked to them, what was said, and when to follow up next. Over 20,000,000 contacts are tracked using Highrise.');
 		    });
 		});
