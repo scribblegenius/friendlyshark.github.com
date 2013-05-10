7b9f50a44436fd35e1e35e7cab2ed61b030ff81c
Chaitanya Vaidya <cvaidya1986@gmail.com>
2013-05-09 22:47:17 -0500

    apos

--- 37signals/application.js
+++ 37signals/application.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
 		$('#subHeadline').text('Frustration-free web-based apps for collaboration, sharing information, and making decisions.');
 		 $('#Headline').removeClass('smaller');
 		    };
-	   var apos = '"\'"'; 
+	   var apos = "'"; 
 
 	  $('#bc').on('mouseenter',function(){
 	    $('#Headline').text('Basecamp is the project management tool you wish you had on your last project.');
@@ -13,14 +13,14 @@ $(document).ready(function(){
 		$('#subHeadline').text('Are you still managing projects with email? Are you still using Excel for your to-do lists? It’s time to upgrade to Basecamp. Manage projects and collaborate with your team and clients the modern way.');
 		    });
 	  $('#hr').on('mouseenter',function(){
-	    $('#Headline').text("Highrise remembers the important things about people you\’d normally forget.");
+	    $('#Headline').text("Highrise remembers the important things about people you"+ apos +"d normally forget.");
 	    $('#Headline').toggleClass('smaller');
 		$('#subHeadline').text('Keep a permanent record of people you do business with. Know who you talked to, when you talked to them, what was said, and when to follow up next. Over 20,000,000 contacts are tracked using Highrise.');
 		    });
 	    $('#cf').on('mouseenter',function(){
 	    $('#Headline').text('From near or far, Campfire helps teams work together over the web in real-time');
 	    $('#Headline').toggleClass('smaller');
-		$('#subHeadline').text("Share ideas, discussions, concepts, images, code samples, videos, mockups, and documents in a real-time private chat room. It"+ apos +"s game changing. We couldn\’t run our own business without Campfire.");
+		$('#subHeadline').text("Share ideas, discussions, concepts, images, code samples, videos, mockups, and documents in a real-time private chat room. It"+ apos +"s game changing. We couldn"+ apos +"t run our own business without Campfire.");
 		    });
 
 	    $('.apps').find('div').on('mouseleave',reset);
