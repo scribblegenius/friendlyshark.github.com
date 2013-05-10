952fede2eff012ba176ab891598ab17a98487846
Chaitanya Vaidya <cvaidya1986@gmail.com>
2013-05-09 22:41:20 -0500

    apostrophe

--- 37signals/application.js
+++ 37signals/application.js
@@ -13,7 +13,7 @@ $(document).ready(function(){
 		$('#subHeadline').text('Are you still managing projects with email? Are you still using Excel for your to-do lists? It’s time to upgrade to Basecamp. Manage projects and collaborate with your team and clients the modern way.');
 		    });
 	  $('#hr').on('mouseenter',function(){
-	    $('#Headline').text('Highrise remembers the important things about people you\’d normally forget.');
+	    $('#Headline').text("Highrise remembers the important things about people you\’d normally forget.");
 	    $('#Headline').toggleClass('smaller');
 		$('#subHeadline').text('Keep a permanent record of people you do business with. Know who you talked to, when you talked to them, what was said, and when to follow up next. Over 20,000,000 contacts are tracked using Highrise.');
 		    });
