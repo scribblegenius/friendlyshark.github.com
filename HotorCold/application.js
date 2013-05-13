bf4bfe41b35b4a938acc6eb562a5c4efdde7498e
Chaitanya Vaidya <cvaidya1986@gmail.com>
2013-05-13 18:07:21 -0500

    playscreen

--- HotorCold/application.js
+++ HotorCold/application.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	$('#playbutton').on('click',function(){ 
 		$(this).toggleClass('playclick');
-		$('.PlayScreen').removeClass().toggleClass('PlayScreenShow').slideDown();
+		$('.PlayScreen').slideToggle();
 	});
 });
