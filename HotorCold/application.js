c8be957668d2b7801e7127e663a0d47657aeebce
Chaitanya Vaidya <cvaidya1986@gmail.com>
2013-05-15 18:11:09 -0500

    setrandom

--- HotorCold/application.js
+++ HotorCold/application.js
@@ -1,6 +1,20 @@
 $(document).ready(function(){
+	var randomnumber = -1;
+
+    function setRandom()
+    {
+    	if(randomnumber == -1)
+    		randomnumber = Math.floor(Math.random()*101);
+    	else
+    		randomnumber = -1;
+    	$('#test').text(randomnumber);
+    }
+
 	$('#playbutton').on('click',function(){ 
 		$(this).toggleClass('playclick');
 		$('.PlayScreen').slideToggle();
+		setRandom();
 	});
+
+	
 });
