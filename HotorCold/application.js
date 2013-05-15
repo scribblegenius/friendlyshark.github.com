f80b510bf3abe7f279ba698b7cd979f025e8c9ca
Chaitanya Vaidya <cvaidya1986@gmail.com>
2013-05-15 18:40:32 -0500

    Updated CheckGuess

--- HotorCold/application.js
+++ HotorCold/application.js
@@ -1,24 +1,50 @@
 $(document).ready(function(){
 	var randomnumber = -1;
     var userguess =-1;
-    function setRandom()
+    var prevuserguess = -1;
+    function InitializeGame()
     {
+    	userguess =-1;
+    	prevuserguess = -1;
     	if(randomnumber == -1)
+    	{
     		randomnumber = Math.floor(Math.random()*101);
+    	}
     	else
+    	{
     		randomnumber = -1;
-    	$('#test').text(randomnumber);
+    	}
     }
+    function CheckUserGuess(guess)
+    {
+    	prevuserguess = userguess;
+    	userguess = guess;
+    	if(userguess == randomnumber){
+    		AnnounceStatus("Victory! You guessed it right! Guess again? ");
+    		InitializeGame();
+    	}
+    	else if(Math.abs(randomnumber-userguess) > Math.abs( randomnumber - prevuserguess))
+    	{
+    		AnnounceStatus("Hot!");
+    	}
+    	else
+    	{
+    		AnnounceStatus("Cold!");
+    	}
 
+    }
+	function AnnounceStatus(text)
+	{
+		$('#Announce').text(randomnumber);
+	}
 	$('#playbutton').on('click',function(){ 
 		$(this).toggleClass('playclick');
 		$('.PlayScreen').slideToggle();
-		setRandom();
+		InitializeGame();
 	});
 
 	$('#submit').on('click',function(){ 
-		userguess = $('#UserGuessTextBox').val();
-		$('#test').text(randomnumber+" "+userguess);
+		CheckUserGuess($('#UserGuessTextBox').val());
 	});
 
 
