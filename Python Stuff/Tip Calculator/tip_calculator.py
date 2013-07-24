802a632868dedaef1e9a950be9501a7da4cc4337
Chaitanya Vaidya <cvaidya1986@gmail.com>
2013-07-24 07:56:14 -0500

    Tip Calc V2

--- Python Stuff/Tip Calculator/tip_calculator.py
+++ Python Stuff/Tip Calculator/tip_calculator.py
@@ -1,12 +1,17 @@
-meal = 20
-tax = 0.15
-tip = 0.20
-tax_value = (meal*tax)
-meal_with_tax =  meal + tax_value
-tip_value = meal_with_tax * tip
-total = meal_with_tax + tip_value
-print "python tip_calculator.py"
-print "The base cost of of your meal was $%s" % meal
-print "You need to pay $%s for tax." % (tax_value)
-print "Tipping at a rate of {0}%, you should leave ${1} for a tip.".format( tip*100,tip_value)
-print "The grand total of your meal is $%s."% (total)
+import sys
+try:
+	meal = float(raw_input('Enter price of meal before tax: '))
+	tax = float(raw_input('Enter tax percentage: '))/100
+	tip = float(raw_input('Enter tip percentage: '))/100
+	tax_value = (meal*tax)
+	meal_with_tax =  meal + tax_value
+	tip_value = meal_with_tax * tip
+	total = meal_with_tax + tip_value
+	print "python tip_calculator.py"
+	print "The base cost of of your meal was $%s" % meal
+	print "You need to pay $%s for tax." % (tax_value)
+	print "Tipping at a rate of {0}%, you should leave ${1} for a tip.".format( tip*100,tip_value)
+	print "The grand total of your meal is $%s."% (total)
+except Exception as e:
+	print " Input values should be numbers! \n Exiting..."
+sys.exit(0)
