cf3097317a41ef29905e65cfa478fb7a76c51266
Chaitanya Vaidya <cvaidya1986@gmail.com>
2013-07-25 20:38:26 -0500

    Tip Calculator V4

--- Python Stuff/Tip Calculator/tip_calculator.py
+++ Python Stuff/Tip Calculator/tip_calculator.py
@@ -1,9 +1,15 @@
+from optparse import OptionParser
 import sys
 try:
-	meal = float(sys.argv[1])
-	tax = float(sys.argv[2])/100
-	tip = float(sys.argv[3])/100
-	tax_value = (meal*tax)
+	parser = OptionParser()
+	parser.add_option("-m", "--meal", dest="meal", help="base meal cost")
+	parser.add_option("-x", "--tax", dest="tax", help="tax percentage")
+	parser.add_option("-t", "--tip", dest="tip", help="tip percentage")
+	(options,args) = parser.parse_args()
+	meal = float(options.meal)
+	tax = float(options.tax)/100
+	tip = float(options.tip)/100
+	tax_value = meal * tax
 	meal_with_tax =  meal + tax_value
 	tip_value = meal_with_tax * tip
 	total = meal_with_tax + tip_value
@@ -13,5 +19,4 @@ try:
 	print "Tipping at a rate of {0}%, you should leave ${1} for a tip.".format( tip*100,tip_value)
 	print "The grand total of your meal is $%s."% (total)
 except Exception as e:
-	print " Input values should be numbers! \n Exiting..."
-sys.exit(0)
+	print "Inputs should be numbers!\nExiting..."
