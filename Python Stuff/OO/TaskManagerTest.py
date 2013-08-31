45823f38adaf7aefd04088980f5cbc021ea230b0
Chaitanya Vaidya <cvaidya1986@gmail.com>
2013-08-31 16:10:02 -0500

    OO add

--- Python Stuff/OO/TaskManagerTest.py
+++ Python Stuff/OO/TaskManagerTest.py
@@ -8,8 +8,8 @@ class TaskManagerTest(unittest.TestCase):
 		self.tmA = TaskManager("A")
 		self.tmB = TaskManager("B")
 		print "--------------------------------------------------------------\n"
-	def test_Adding_Deleting_Printing(self):       #must start with test_...
-		print "Test 4: Multiple Add , Delete and Print\n"
+	def test_1Adding_Deleting_Printing_Works(self):       #must start with test_...
+		print "Test 1: Multiple Add , Delete and Print\n"
 		tm = self.tmA
 		tm.add_task(Task("Buy milk","Done","Trivial"))
 		tm.add_task(UrgentTask("Pay rent","Done"))
@@ -23,12 +23,12 @@ class TaskManagerTest(unittest.TestCase):
 		tm.add_task(ImportantTask("Learn Python Classes","Pending"))
 		print self.tmB
 		self.assertEqual(len(tm.tasklist),2)
-	def test_Delete_Non_Existent_Task(self):
-		print "Test 5: Delete Non-Existent Task\n"
+	def test_2Delete_Non_Existent_Tasks_Foiled(self):
+		print "Test 2: Delete Non-Existent Task\n"
 		tm = self.tmA
 		tm.del_task(100)
-	def test_Multitask(self):
-		print "Test 2: Check if multiple taskmanagers can be created\n"
+	def test_3Multiple_Taskmanagers_Allowed(self):
+		print "Test 3: Check if multiple taskmanagers can be created\n"
 		self.tmA.add_task(Task("Buy milk","Done","Trivial"	))
 		self.tmA.add_task(Task("Buy car","Done","Important"	))
 		self.tmA.add_task(Task("Buy jet","Pending","Trivial"	))
@@ -38,13 +38,13 @@ class TaskManagerTest(unittest.TestCase):
 		print "\n\n"
 		print self.tmB 
 		self.assertNotEqual(self.tmA.size() , self.tmB.size())
-	def test_NumberInputs(self):
+	def test_4Numerical_Inputs_Default_Gracefully(self):
 		args = [1,1.4,100000000]
-		print "Test 3: Handle special input {0} \n".format(args)
+		print "Test 4: Handle special input {0} \n".format(args)
 		self.tmA.add_task(Task(*args))
 		self.assertEqual(self.tmA.size(),1)
-	def test_Duplicate_Add_Not_Allowed(self):
-		print "Test 1: Handle Duplicate Task Adds\n"
+	def test_5Duplicate_Task_Add_Not_Allowed(self):
+		print "Test 5: Handle Duplicate Task Adds\n"
 		self.tmA.add_task(Task("Pay monthly electricity bill","Done","Urgent"))
 		self.tmA.add_task(Task("   pay MONTHLy ELECTRICity BIll   ","Done","Urgent"))
 		self.assertEqual(self.tmA.size(),1)
